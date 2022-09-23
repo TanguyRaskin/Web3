404801fd07e9f2232bde1040f25bb7b3b655ddf9
TanguyRaskin <Talapoin981014?>
2022-09-23 10:21:36 +0200

    1.1-1.2 : import image

--- exercices/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercices/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,8 +1,14 @@
+import image from './LOGO HE VINCI.png'
+
+
 const Header = (props) => {
+    const logo = image;
+
     return (
       <>
+        <img src={logo} alt="logo" />
         <h1>{props.course}</h1>
       </>
     )
   }
-  export default Header
+  export default Header;
